b8e2bb7f9d5594f1f89910eaa22e1475f74c6e49
kidproko22223 <kidproko22223@gmail.com>
2019-12-01 18:07:55 +0700

    Change interface and function

---
+++ QuizApplication/Person.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+
+namespace QuizApplication
+{
+    public class Person
+    {
+        public Person()
+        {
+            Name = "";
+            Time = 0;
+            Age = 18;
+            IsMale = true;
+        }
+
+        public string Name { get; set; }
+        public float Accuracy { get; set; }
+        public int Time { get; set; }
+        public int Age { get; set; }
+        public bool IsMale { get; set; }
+        public string TimeForHumans
+        {
+            get
+            {
+                return ControlMethods.GetTimeInHumanLanguage(Time);
+            }
+        }
+        public string Level
+        {
+            get
+            {
+                return ControlMethods.GetLevelString(Accuracy);
+            }
+        }
+        public string Comment
+        {
+            get { return DataLayer.GetComment(Accuracy); }
+        }
+        public string FirstName
+        {
+            get
+            {
+                if (Name.Contains(' '))
+                {
+                    return Name.Split(' ')[0];
+                }
+                else
+                    return Name;
+            }
+        }
+    }
+}
---
+++ QuizApplication/Question.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace QuizApplication.Logic
+{
+    public class Question
+    {
+        #region Construction
+        public Question(XElement data, int level)
+        {
+
+            if (data == null)
+                throw new ArgumentNullException("data");
+
+            AllAnswers = new List<Answer>();
+            Title = ControlMethods.ApplyMeaningfulChars(ControlMethods.SelectXElement("title", data).Value);
+
+            int index = 0;
+            foreach (XElement answer in ControlMethods.SelectXElement("answers", data).Descendants())
+            {
+                var newAnswer = new Answer(answer.Value, index);
+
+                if (ControlMethods.SelectXAttribute("correct", answer) != null &&
+                    ControlMethods.SelectXAttribute("correct", answer).Value.ToLower() == "true")
+                    newAnswer.IsTheCorrectAnswer = true;
+
+                AllAnswers.Add(newAnswer);
+
+                index++;
+            }
+
+            Level = level;
+        }
+        #endregion // Construction
+
+        #region Public Properties
+        public string Title { get; set; }
+        public List<Answer> AllAnswers { get; set; }
+        public int CorrectAnswer
+        {
+            get
+            {
+                foreach (var ans in AllAnswers)
+                {
+                    if (ans.IsTheCorrectAnswer)
+                        return ans.Index;
+                }
+
+                throw new Exception("No answer is marked as correct.");
+            }
+
+        }
+        public string Index { get; set; }
+        public int UserAnswer
+        {
+            get
+            {
+                foreach (var ans in AllAnswers)
+                {
+                    if (ans.IsChosenByUser)
+                        return ans.Index;
+                }
+
+                return -1;
+            }
+        }
+        public int Level { get; set; }
+        #endregion // Public Properties
+
+        #region Public Overrided Methods
+        public override string ToString()
+        {
+            return Title;
+        }
+        #endregion
+    }
+}
